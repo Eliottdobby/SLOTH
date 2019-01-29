ea79246ea8f2d992af8f6bee44c4d52388d6e13e
Mathieu Schaller <mathieuschaller@MacBook-Pro-de-Mathieu.local>
2019-01-29 14:16:08 +0100

    ajout de fill_table.sql

---
+++ SLOTH/SQL-BDD/fill_table.sql
@@ -0,0 +1,11 @@
+insert into utilisateur ( utilisateur_id, utilisateur_nom,utilisateur_prenom) 
+	values
+	('chamon', 'Hamon','Célestin'),
+	('mschalle', 'Schaller','Mathieu'),
+	('hmarjoux', 'Marjoux','Hoel'),
+	('clescure', 'Lescure','Camille'),
+	('epourrat', 'Pourrat','Eliott'); 
+
+
+
+
